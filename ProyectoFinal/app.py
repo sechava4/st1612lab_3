3fba640b60a29dbbfd1c3591dbf15be9e82a5eb9
sechava4 <sechava4@eafit.edu.co>
2021-05-09 22:24:15 -0500

    fix get method

--- ProyectoFinal/app.py
+++ ProyectoFinal/app.py
@@ -42,28 +42,31 @@ def add_entry():
     else:
         args = request.args.to_dict()
     if args:
-        data = args
-        data["timestamp"] = str(datetime.datetime.utcnow())
+        if float(args["latitude"]) > 0:
+            data = args
+            data["timestamp"] = str(datetime.datetime.utcnow())
 
-        data["coords"] = (float(args["latitude"]), float(args["longitude"]))
-        google_url = (
-                "https://maps.googleapis.com/maps/api/elevation/json?locations="
-                + str(args["latitude"])
-                + ","
-                + str(args["longitude"])
-                + "&key=AIzaSyChV7Sy3km3Fi8hGKQ8K9t7n7J9f6yq9cI"
-        )
+            data["coords"] = (float(args["latitude"]), float(args["longitude"]))
+            google_url = (
+                    "https://maps.googleapis.com/maps/api/elevation/json?locations="
+                    + str(args["latitude"])
+                    + ","
+                    + str(args["longitude"])
+                    + "&key=AIzaSyChV7Sy3km3Fi8hGKQ8K9t7n7J9f6yq9cI"
+            )
 
-        r = requests.get(google_url).json()
+            r = requests.get(google_url).json()
 
-        data["elevation"] = r["results"][0]["elevation"]
-        try:
-            prod = get_kafka_client()
-            prod.send('vehicle', value=str.encode(data))
-        except Exception as e:
-            print(e)
+            data["elevation"] = r["results"][0]["elevation"]
+            try:
+                prod = get_kafka_client()
+                prod.send('vehicle', value=str.encode(data))
+            except Exception as e:
+                print(e)
 
-        return json.dumps(data)
+            return json.dumps(data)
+        args["status"] = "invalid location"
+        return json.dumps(args)
     return dict(data="empty")
 
 
